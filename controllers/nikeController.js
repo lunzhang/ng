955c000f1c7f5b63eddf25acad90b9643acbc33d
lunzhang <breakermonkey55@yahoo.com>
2016-03-30 12:47:44 -0400

    fixed algo

--- controllers/nikeController.js
+++ controllers/nikeController.js
@@ -1,6 +1,7 @@
 ﻿app.controller('nikeController', function ($scope, $window, $http) {
     
     var oldShoes = $window.localStorage.getItem('oldShoes');
+    var prevNewShoes = $window.localStorage.getItem('prevNewShoes');
     var currentShoes = [];
     var newShoes = [];
     var dataURL = 'http://store.nike.com/html-services/gridwallData?country=US&lang_locale=en_US&gridwallPath=mens-clearance-shoes/47Z7puZoi3&sortOrder=publishdate|desc';
@@ -9,11 +10,15 @@
     if (!oldShoes) {
         oldShoes = [];
     }
-
+    if (!prevNewShoes) {
+        prevNewShoes = [];
+    }
     if (oldShoes.length > 0) {
         oldShoes = JSON.parse(oldShoes);
     }
-        
+    if (prevNewShoes.length > 0) {
+        prevNewShoes = JSON.parse(prevNewShoes);
+    }  
     getShoes(dataURL);
 
     function getShoes(url) {
@@ -31,13 +36,31 @@
                     getShoes(dataURL);
                     counter++;
                 }
-                else {                    
-                    $window.localStorage.setItem('oldShoes', JSON.stringify(currentShoes));                    
-                    
+                else {              
+                    currentShoes.push({ title: 'KD 8 iD' });                         
                     for (var i = 0; i < currentShoes.length; i++) {
-                        if (currentShoes[i].title != oldShoes[i].title) {
-                            newShoes.push(currentShoes[i]);
+                        var currentShoe = currentShoes[i];
+                        var exist = false;
+
+                        for (var j = 0; j < oldShoes.length; j++) {
+                            var oldShoe = oldShoes[j];
+                            if (currentShoe.title == oldShoe.title) {
+                                exist = true;
+                            }
                         }
+                        
+                        if (!exist) {
+                            newShoes.push(currentShoe);
+                        }
+
+                    }
+                    
+                    if (newShoes.length > 0) {
+                        $window.localStorage.setItem('prevNewShoes', JSON.stringify(newShoes));
+                        $window.localStorage.setItem('oldShoes', JSON.stringify(currentShoes));  
+                    }
+                    else {
+                        newShoes = prevNewShoes;
                     }
 
                     $scope.oldShoes = oldShoes;
