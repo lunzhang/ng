3c7fad78af8d94940b65eecbcb07865298fb88d6
lunzhang <lun.zhang1993@gmail.com>
2016-08-02 12:31:44 -0400

    refactored blog and d3

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -1,6 +1,8 @@
-app.controller('blogController',['$scope','$location','$anchorScroll', function($scope,$location,$anchorScroll ) {
-  $scope.scrollTo = function(id) {
-      $location.hash(id);
-      $anchorScroll();
-   }
+app.controller('blogController',['$scope', function($scope) {
+  var mapProp = {
+   center:new google.maps.LatLng(51.508742,-0.120850),
+   zoom:5,
+   mapTypeId:google.maps.MapTypeId.ROADMAP
+ };
+ var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }]);
--- controllers/d3.controller.js
+++ controllers/d3.controller.js
@@ -1,92 +1,234 @@
 ﻿app.controller('d3Controller', ['$scope', '$http', function ($scope, $http) {
-        
-        (function nyc(){
-            var width = 480,
-                height = 600;
-            var projection = d3.geo.transverseMercator()
-    .rotate([74 + 30 / 60, -38 - 50 / 60]);
-            var path = d3.geo.path()
-    .projection(projection);
-            
-            var svg = d3.select("#nyc").append("svg")
-    .attr("width", width)
-    .attr("height", height);
-        })();    
 
-        function lineChart() {
-            
-            var activeRates = ['bidOpen'];
-            var rates;
-            var margin = { top: 20, right: 20, bottom: 30, left: 50 },
-                height = 500 - margin.left - margin.right,
-                width = 960 - margin.top - margin.bottom;
-            
-            var x = d3.time.scale().range([0, width]);
-            
-            var y = d3.scale.linear().range([height, 0]);
-            
-            var xAxis = d3.svg.axis().scale(x).orient("bottom");
-            
-            var yAxis = d3.svg.axis().scale(y).orient("left");
-            
-            function ratesLineByValue(rates, value) {
-                var line = d3.svg.line().x(function (d) { return x(d.date); }).y(function (d) { return y(d[value]); });
-                return line(rates);
-            }
-            
-            function ratesLineByIndex(rates, i) {
-                var line = d3.svg.line().x(function (d) { return x(d.date); }).y(function (d) { return y(d[activeRates[i]]); });
-                return line(rates);
-            }
-            
-            var svg = d3.select("#chart").append("svg").attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom).append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-            
-            svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + height + ")");
-            svg.append("g").attr("class", "y axis");
-            
-            function getRate(period) {
-                svg.selectAll('.line').remove();
-                $http.get('/api/rates/' + period).then(function (resp) {
-                    
-                    rates = resp.data.rates;
-                    rates.forEach(function (d) {
-                        d.date = d.date * 1000;
-                    });
-                    x.domain(d3.extent(rates, function (d) { return d.date; }));
-                    y.domain([d3.min(rates, function (d) { return Math.min(d.bidOpen, d.bidClose, d.bidHigh, d.bidLow, d.askOpen, d.askClose, d.askHigh, d.askLow); })
-                    , d3.max(rates, function (d) { return Math.max(d.bidOpen, d.bidClose, d.bidHigh, d.bidLow, d.askOpen, d.askClose, d.askHigh, d.askLow); })]);
-                    
-                    svg.select('.x.axis').call(xAxis);
-                    
-                    svg.select('.y.axis').call(yAxis);
-                    
-                    for (var i = 0; i < activeRates.length; i++) {
-                        svg.append('path').attr('class', 'line').attr('id', activeRates[i]).attr('d', ratesLineByIndex(rates, i));
-                    }
+  //globe
+  (function () {
+      //global variables
+      var winWidth = $(window).width();
+      if (winWidth < 400) {
+          var width = 180,
+              height = 180;
+          var projection = d3.geo.orthographic()
+          .scale(90)
+          .translate([width / 2, height / 2])
+          .clipAngle(90)
+          .precision(.1);
+      } else {
+          var width = 360,
+              height = 360;
+          var projection = d3.geo.orthographic()
+          .scale(180)
+          .translate([width / 2, height / 2])
+          .clipAngle(90)
+          .precision(.1);
+      }
 
-                });
-            }
-            
-            $('#period :radio').change(function (e) {
-                var period = $(this).val();
-                getRate(period);
-            });
-            
-            $('#rates :checkbox').change(function (e) {
-                var checkbox = $(this);
-                var rate = checkbox.val();
-                if (checkbox.is(':checked')) {
-                    activeRates.push(rate);
-                    svg.append('path').attr('class', 'line').attr('id', rate).attr('d', ratesLineByValue(rates, rate));
-                }
-                else {
-                    activeRates.splice(activeRates.indexOf(rate), 1);
-                    svg.select('#' + rate).remove();
-                }
-            });
-            
-            getRate('M1');
-        }
+      var path = d3.geo.path()
+          .projection(projection);
+      var graticule = d3.geo.graticule();
+      //appends svg
+      var globe = d3.select("#globe").append("svg")
+          .attr("width", width)
+          .attr("height", height);
+      var g = globe.append("g").attr("zoom", false);
+      //appends sphere
+      g.append("defs").append("path")
+          .datum({ type: "Sphere" })
+          .attr("id", "sphere")
+          .attr("d", path);
+      //appends fill
+      g.append("use")
+          .attr("class", "fill")
+          .attr("xlink:href", "#sphere");
+      //loads world map and country names
+      queue()
+          .defer(d3.json, "/lib/d3/world-110m.json")
+          .defer(d3.tsv, "/lib/d3/world-country-names.tsv")
+          .defer(d3.csv, "/lib/d3/locations.csv")
+          .await(ready);
+      //when loaded
+      function ready(error, world, names, locations) {
+          if (error) throw error;
+          //add country names
+          var countries = topojson.feature(world, world.objects.countries).features;
+          countries = countries.filter(function (d) {
+              return names.some(function (n) {
+                  if (d.id == n.id) return d.name = n.name;
+              });
+          }).sort(function (a, b) {
+              return a.name.localeCompare(b.name);
+          });
+          //land
+          g.selectAll("path.land")
+              .data(countries)
+              .enter().append("path")
+              .attr("class", "land")
+              .attr("d", path)
+              .attr("name", function (d, i) {
+              return name = d.name;
+          })
+              .attr("i", function (d, i) {
+              return i = i;
+          });
+
+          //boundaries
+          g.insert("path")
+              .datum(topojson.mesh(world, world.objects.countries, function (a, b) { return a !== b; }))
+              .attr("class", "boundary")
+              .attr("d", path);
+
+          var inProgress = false;
+          var currentCountry;
+
+          g.selectAll("circle")
+          .data(locations)
+          .enter()
+          .append("circle")
+          .attr("cx", function (d) {
+              return projection([d.lon, d.lat])[0];
+          })
+          .attr("cy", function (d) {
+              return projection([d.lon, d.lat])[1];
+          })
+          .attr("id", function (d) {
+              return d.location;
+          })
+          .style('fill', '#E43131')
+          .style('display', 'none');
+
+          var tip;
+          var tooltip = d3.select('#tooltip');
+          this.zoomIn = function (i, location) {
+              var bounds = path.bounds(countries[i]),
+                  dx = bounds[1][0] - bounds[0][0],
+                  dy = bounds[1][1] - bounds[0][1],
+                  x = (bounds[0][0] + bounds[1][0]) / 2,
+                  y = (bounds[0][1] + bounds[1][1]) / 2,
+                  scale = .9 / Math.max(dx / width, dy / height),
+                  translate = [width / 2 - scale * x, height / 2 - scale * y];
+              g.transition()
+                  .duration(750)
+                  .attr("transform", "translate(" + translate + ")scale(" + scale + ")").each("end", function () {
+                  g.attr("zoom", true);
+                  currentCountry = countries[i].name;
+                  inProgress = false;
+                  var circle = '#' + location;
+                  g.select(circle).style('display', 'inline');
+
+                  var posX = $(circle).offset().left-100;
+                  var posY = $(circle).offset().top - 100;
+                  tooltip.style("left", (posX) + "px")
+                             .style("top", (posY) + "px")
+                              .style("display", 'block');
+
+              });
+          };
+
+          this.transition = function (i, location) {
+              d3.transition()
+              .duration(1000)
+              .tween("rotate", function () {
+                  var p = d3.geo.centroid(countries[i]),
+                      r = d3.interpolate(projection.rotate(), [-p[0], -p[1]]);
+                  return function (t) {
+                      projection.rotate(r(t));
+                      g.selectAll("path").attr("d", path)
+                                      .classed("focused", function (d, i) {
+                      });
+                      g.selectAll("circle")
+                      .attr("cx", function (d) {
+                          return projection([d.lon, d.lat])[0];
+                      })
+                      .attr("cy", function (d) {
+                          return projection([d.lon, d.lat])[1];
+                      });
+                  };
+              }).transition().each("end", function () {
+                  zoomIn(i, location);
+              });
+          }
+
+          this.zoomOut = function (i, location, datatip) {
+              $scope.tip = datatip;
+              $scope.$apply();
+              if (!inProgress) {
+                  if (g.attr("zoom") === "true" && currentCountry != countries[i].name) {
+                      tooltip.style("display", 'none');
+                      inProgress = true;
+                      g.transition()
+                          .duration(750)
+                          .attr("transform", "translate(0,0) scale(1,1)").each("end", function () {
+                          g.selectAll('circle').style('display', 'none');
+                          transition(i, location);
+                      });
+                  }
+                  else if (g.attr("zoom") === "false") {
+                      inProgress = true;
+                      g.selectAll('circle').style('display', 'none');
+                      transition(i, location);
+                  }
+                  else {
+                      g.selectAll('circle').style('display', 'none');
+                      g.select('#' + location).style('display', 'inline');
+                  }
+              }
+          }
+
+      };
+
+  })();
+
+  //timeline
+  (function () {
+      var data = [
+          {
+              times: [
+                  { "starting_time": 757287682200, "display" : "circle", "id": "born", "index": 31, "location": "wz", "tip" : "Born in Wz,CN on 11/30/1993" }]
+          },
+          {
+              times: [
+                  { "starting_time": 969655282200, "display": "circle", "id": "us", "index": 164, "location": "nyc", "tip" : "Arrived in Nyc,US on 8/22/2000" }]
+          },
+          {
+              times: [
+                  { "starting_time": 1224016882200, "display": "circle", "id": "hs", "index": 164, "location": "nyc", "tip" : "Started Bronx Science HS on 9/14/2008" }]
+          },
+          {
+              times: [
+                  { "starting_time": 1348260082200, "display": "circle", "id": "college", "index": 164, "location": "sb", "tip" : "Started Stony Brook on 8/21/2012" }]
+          },
+          {
+              times: [
+                  { "starting_time": 1435178482200, "display": "circle", "id": "bluum", "index": 164, "location": "nyc", "tip" : "Intern at Bluum on 5/24/2015 to 8/12/2015" }]
+          },
+          {
+              times: [
+                  { "starting_time": 1453585282200, "display": "circle", "id": "grad", "index": 164, "location": "sb", "tip" : "Graduated Stony Brook on 12/23/2015" }]
+          },
+          {
+              times: [
+                  { "starting_time": 1458679282200, "display": "circle", "id": "fxcm", "index": 164, "location": "nyc", "tip" : "Started Fxcm on 2/22/2016" }]
+          }
+      ];
+      //tooltip
+      var tooltip = d3.select('#tooltip');
+      var chart = d3.timeline()
+             .tickFormat({
+          format: d3.time.format("%Y"),
+          tickTime: d3.time.years,
+          tickInterval: 1,
+          tickSize: 3
+      })
+              .width('1000')
+              .beginning(757287682200)
+              .ending(1458679282200)
+              .mouseover(function (d, i, datum) {
+          zoomOut(d.index, d.location,d.tip);
+      })
+              .mouseout(function (d, i, datum) {
+
+      });
+      var timeline = d3.select("#timeline").append("svg").attr("width", '1000')
+              .datum(data).call(chart);
+  })();
+
     }]);
