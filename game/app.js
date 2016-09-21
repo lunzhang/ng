8aa69fe4bf7338d4ed804a65c1c145e42902db08
lunzhang <lun.zhang1993@gmail.com>
2016-09-21 12:27:36 -0400

    downgraded jQuery to v2.2.4

--- game/app.js
+++ game/app.js
@@ -1,6 +1,11 @@
 var game = (function(){
 
-  
+
+  function start(){
+    game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-container', { preload: preload, create: create, update: update });
+  }
+
+
   return {
     start:start
   };
